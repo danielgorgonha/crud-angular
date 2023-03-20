3d63c9f4f00891f20bc30904525d9b51918184bd
Daniel R Gorgonha <rogergorgonha@gmail.com>
2023-03-20 20:04:34 -0300

    feat: add request http get + rxjs

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,8 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -9,12 +11,14 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category']
 
-  constructor(private coursesService: CoursesService) {}
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+
   }
 }
